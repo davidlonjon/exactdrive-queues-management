c03d795262f2a75ad562875f0b8eac772051959e
David Lonjon <david.lonjon@gmail.com>
2016-06-06 21:55:10 +0900

    Start to implement AppNexus Campaign sync jobs

---
+++ app/Http/Controllers/CampaignController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Jobs\AppNexusCampaignJob;
+use Exactdrive\AppNexus;
+use Illuminate\Queue\Queue;
+
+class CampaignController extends Controller
+{
+
+    /**
+     * The user repository instance.
+     */
+    protected $queue;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->queue = app('queue');
+    }
+
+    /**
+     * Controller for the route handling syncing AppNexus campaign domain
+     *
+     * @param int $campaignId Campaign Id
+     */
+    public function syncAppNexusDomains($campaignId)
+    {
+        $payload = $this->createJobCorePayload();
+        $payload['body']['action'] = 'syncAppNexusDomains';
+        $payload['body']['data'] = array(
+            'campaignId' => intval($campaignId),
+        );
+
+        $this->queue->push(new AppNexusCampaignJob($payload));
+
+        return response()->json(['status' => 'ok', 'message' => 'syncAppNexusDomains request sent to queue']);
+    }
+}
---
+++ app/Jobs/AppNexusCampaignJob.php
@@ -0,0 +1,297 @@
+<?php
+
+namespace App\Jobs;
+
+use Exactdrive\AppNexus;
+
+class AppNexusCampaignJob extends AppNexusBaseJob
+{
+
+    private $payload;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($payload)
+    {
+        $this->payload = $payload;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+
+        $this->configureAppNexus();
+
+        $response = $this->createCoreResponse($this->payload);
+
+        if (!isset($this->payload['body'], $this->payload['body']['action'], $this->payload['body']['data'])) {
+            $response['code'] = 'malformedPayload';
+            $response['message'] = 'The payload is malformed';
+
+            $this->dispatchError($response);
+        }
+
+        $jobAction = $this->payload['body']['action'];
+
+        switch ($jobAction) {
+            case 'syncAppNexusDomains':
+                $response = $this->syncAppNexusDomains($this->payload);
+                break;
+
+            // case 'syncAppNexusCampaignProfile':
+            //     $response = $this->syncAppNexusCampaignProfile($this->payload);
+            //     break;
+
+            // case 'syncAppNexusLineItem':
+            //     $response = $this->syncAppNexusLineItem($this->payload);
+            //     break;
+
+            // case 'syncAppNexusCampaign':
+            //     $response = $this->syncAppNexusCampaign($this->payload);
+            //     break;
+
+            // case 'syncStatus':
+            //     $response = $this->syncStatus($this->payload);
+            //     break;
+
+            // case 'deleteCampaign':
+            //     $response = $this->deleteCampaign($this->payload);
+            //     break;
+
+            default:
+                break;
+        }
+
+        dump($response);
+    }
+
+    /**
+     * Job to sync campaign domains
+     *
+     * @param array $payload Payload
+     */
+    private function syncAppNexusDomains($payload = array())
+    {
+        $response = $this->createCoreResponse($this->payload);
+
+        if (!isset($payload['body']['data']['campaignId'])) {
+            $response['code'] = 'missingParameter';
+            $response['message'] = 'Missing campaign ID parameter';
+
+            $this->dispatchError($response);
+        }
+
+        $campaignId = intval($payload['body']['data']['campaignId']);
+
+        $campaign = $this->getCampaign($campaignId);
+
+        $data = $this->campaignPreSyncChecks($campaign);
+        if ('error' === $data['status'] && !empty($data['code'])) {
+            $response = $data;
+            return $response;
+        }
+
+        foreach (array('include', 'exclude') as $inventoryUrlFilter) {
+
+            // Build domain array from list of domains in Campaign
+            $inventoryUrlProperty = $inventoryUrlFilter."InventoryUrls";
+            $domains = $campaign->{$inventoryUrlProperty};
+            $domains = preg_replace('/[^A-Za-z0-9\-\.\n]/', '', trim($domains));
+            $domainsArray = explode("\n", $domains);
+
+            if (!empty($domainsArray)) {
+
+                // Build data object
+                $data = new \stdClass();
+                $data->name = "$campaign->name $inventoryUrlFilter list";
+                $data->description = "Domains to $inventoryUrlFilter from campaign $campaign->name";
+
+                $data->domains = $domainsArray;
+
+                if ($inventoryUrlFilter == 'exclude') {
+                    $data->type = 'black';
+                    $appNexusDomainListId = 'appNexusExcludeDomainListId';
+                } else {
+                    $data->type = 'white';
+                    $appNexusDomainListId = 'appNexusIncludeDomainListId';
+                }
+
+                $campaignFieldsToUpdate = array(
+                        'lastSyncedWithAppNexus' => date('Y-m-d H:i:s')
+                );
+
+                if (empty($campaign->{$appNexusDomainListId})) {
+                    $AppNexusResponse = AppNexus\DomainListService::addDomainList($data);
+                    $campaignFieldsToUpdate[$appNexusDomainListId] = $AppNexusResponse->id;
+                } else {
+                    $AppNexusResponse = AppNexus\DomainListService::updateDomainList($campaign->{$appNexusDomainListId}, $data);
+                }
+
+                try {
+                    \DB::table('campaigns')
+                        ->where('id', $campaign->id)
+                        ->update($campaignFieldsToUpdate);
+                } catch (Exception $e) {
+                    $response['code'] = $e->getCode();
+                    $response['message'] = $e->getMessage();
+
+                    $this->dispatchError($response);
+                }
+            }
+        }
+
+        $response['status'] = 'ok';
+        $response['code'] = 'jobSuccessful';
+        $response['message'] = 'AppNexus campaign domains synced';
+        $response['data'] = $campaign;
+        return $response;
+    }
+
+    /**
+     * Campaign pre sync checks
+     *
+     * @param  int $campaign Campaign id
+     *
+     * @return array           Pre checks data
+     */
+    private function campaignPreSyncChecks($campaign) {
+        $data = $this->createCoreResponse(array());
+
+        $user = $this->getAppNexusUserForAdvertiserId($campaign->advertiserId);
+
+        if (!$this->isValidAppNexusAdvertiser($user, $campaign->advertiserId)) {
+            $data['code'] = 'invalidAppNexusAdvertiser';
+            $data['message'] = "Invalid AppNexus Advertiser: $campaign->advertiserId";
+        }
+
+        $data['data']['user'] = $user;
+
+        if (!$this->isValidSyncStatus($campaign->status)) {
+            $data['code'] = 'invalidCampaignStatusSync';
+            $data['message'] = "Invalid Campaign Status: $campaign->status";
+        }
+
+        $inventories = $this->getCampaignInventories($campaign->id);
+        if (empty($inventories)) {
+            $data['code'] = 'emptyInventories';
+            $data['message'] = 'Empty inventories';
+        }
+        $data['data']['inventories'] = $inventories;
+
+        return $data;
+    }
+
+    /**
+     * Get campaign from DB
+     *
+     * @param  int $campaignId User ID
+     *
+     * @return object|null         User
+     */
+    private function getCampaign($campaignId)
+    {
+        $response = $this->createCoreResponse();
+
+        try {
+            $campaign = \DB::table('campaigns')->where('id', $campaignId)->first();
+        } catch (\Exception $e) {
+            $response['code'] = $e->getCode();
+            $response['message'] = $e->getMessage();
+            $this->dispatchError($response);
+        }
+
+        if (!$campaign) {
+            $response['code'] = 'campaignNotFound';
+            $response['message'] = "User $campaignId not found";
+            $this->dispatchError($response);
+        }
+
+        return $campaign;
+    }
+
+    /**
+     * Get campaign inventories
+     *
+     * @param  int $campaignId Campaign id
+     *
+     * @return object             Campaign inventories
+     */
+    private function getCampaignInventories($campaignId)
+    {
+        $inventories = \DB::table('inventories')
+            ->where('campaignId', $campaignId)
+            ->get();
+
+        return $inventories;
+    }
+
+    /**
+     * Get AppNexus advertiser id
+     *
+     * @param  int $advertiserId Advertiser id
+     *
+     * @return int               Advertiser id
+     */
+    private function getAppNexusUserForAdvertiserId($advertiserId)
+    {
+        $response = $this->createCoreResponse();
+
+        try {
+            $user = \DB::table('users')
+                ->join('advertisers', 'users.id', '=', 'advertisers.userId')
+                ->where('advertisers.id', $advertiserId)
+                ->select('users.*')
+                ->first();
+
+            return $user;
+        } catch (\Exception $e) {
+            $response['code'] = $e->getCode();
+            $response['message'] = $e->getMessage();
+            $this->dispatchError($response);
+        }
+    }
+
+    /**
+     * Check whether or not user associated to an Advertiser has an AppNexus Advertiser ID.
+     *
+     * @param  object  $user User
+     * @param  int  $advertiserId Advertiser id
+     *
+     * @return boolean        Returns true if a user has an AppNexus Advertiser ID.
+     */
+    private function isValidAppNexusAdvertiser($user, $advertiserId)
+    {
+        $response = $this->createCoreResponse();
+
+        if (!$user) {
+            $response['code'] = 'userAdvertiserNotFound';
+            $response['message'] = "User related to advertiser $advertiserId not found";
+            $this->dispatchError($response);
+        }
+
+        return !empty($user->appNexusAdvertiserID);
+    }
+
+    /**
+     * Check whether or not a campaign as a valid sync status
+     *
+     * @param  string  $campaignStatus Campaign status
+     *
+     * @return boolean                 Return true if the campaign has a valid sync status
+     */
+    private function isValidSyncStatus($campaignStatus)
+    {
+        return in_array(
+            $campaignStatus,
+            array('active', 'inactive'),
+            true
+        );
+    }
+}
