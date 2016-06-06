adce53f4623c9463b96f840f0a3ae6db05dbe78e
David Lonjon <david.lonjon@gmail.com>
2016-06-06 22:28:10 +0900

    Refactor further the creation of new jobs in campaign routes controller

--- app/Http/Controllers/CampaignController.php
+++ app/Http/Controllers/CampaignController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Jobs\AppNexusCampaignJob;
 use Exactdrive\AppNexus;
 use Illuminate\Queue\Queue;
 
@@ -24,18 +23,6 @@ class CampaignController extends Controller
         $this->queue = app('queue');
     }
 
-    public function createNewJob($job, $campaignId) {
-        $payload = $this->createJobCorePayload();
-        $payload['body']['action'] = $job;
-        $payload['body']['data'] = array(
-            'campaignId' => intval($campaignId),
-        );
-
-        $this->queue->push(new AppNexusCampaignJob($payload));
-
-        return ['status' => 'ok', 'message' => "$job job sent to queue"];
-    }
-
     /**
      * Controller for the route handling syncing AppNexus campaign domain.
      *
@@ -43,7 +30,12 @@ class CampaignController extends Controller
      */
     public function syncAppNexusDomains($campaignId)
     {
-        $response = $this->createNewJob('syncAppNexusDomains', $campaignId);
+        $response = $this->createNewJob(
+            'AppNexusCampaignJob',
+            'syncAppNexusDomains',
+            array('campaignId' => intval($campaignId))
+        );
+
         return response()->json($response);
     }
 }
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,28 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
+
+    /**
+     * Create new AppNexus job.
+     *
+     * @param  string $job       Job class name
+     * @param  string $jobAction Job action
+     * @param  array $data      Job data
+     *
+     * @return array            Route response
+     */
+    public function createNewJob($job, $jobAction, $data)
+    {
+        $payload = $this->createJobCorePayload();
+        $payload['body']['action'] = $jobAction;
+        $payload['body']['data'] = $data;
+
+        $jobClass = "App\\Jobs\\$job";
+        $this->queue->push(new $jobClass($payload));
+
+        return array('status' => 'ok', 'message' => "$jobAction job sent to queue");
+    }
+
     /**
      * Create job core payload.
      *
